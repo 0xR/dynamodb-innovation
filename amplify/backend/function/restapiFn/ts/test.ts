c1f65c96d43b6687343f08c11347d9d56b253abe
Ruben Oostinga <ruben.oostinga@gmail.com>
2022-10-26 16:51:32 +0200

    Add user by email fetching

--- amplify/backend/function/restapiFn/ts/test.ts
+++ amplify/backend/function/restapiFn/ts/test.ts
@@ -4,31 +4,30 @@ import { AccountModel, UserModel } from './one-table';
 
 async function create() {
 
-const account = await AccountModel.create({
-  id: ulid(),
-  name: 'Acme Airplanes',
-  zip: '1234ab'
-});
-
-for (let i = 0; i < 1e2; i++) {
-  await UserModel.create({
+  const account = await AccountModel.create({
     id: ulid(),
-    accountId: account.id,
-    email: faker.internet.email(),
-    firstName: faker.name.firstName(),
-    lastName: faker.name.lastName(),
-    username: faker.internet.userName(),
-    role: 'user',
-    balance: faker.datatype.number({ min: 0, max: 1000000 })
+    name: 'Acme Airplanes',
+    zip: '1234ab'
   });
-}
+
+  for (let i = 0; i < 1e2; i++) {
+    await UserModel.create({
+      id: ulid(),
+      accountId: account.id,
+      email: faker.internet.email(),
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      username: faker.internet.userName(),
+      role: 'user',
+      balance: faker.datatype.number({ min: 0, max: 1000000 })
+    });
+  }
 }
 
 async function query() {
+
+  // get all users by account id
   let accountId = '01GGA9BWMKZAJRN0YEDYM0GF6S';
-  console.log(await AccountModel.get({
-    id: accountId
-  }));
 
   let users = await UserModel.find({
     accountId
@@ -38,6 +37,7 @@ async function query() {
 
   console.log('users1=', users);
 
+  // pagination
   let users2 = await UserModel.find({
     accountId
   }, {
@@ -46,7 +46,16 @@ async function query() {
   });
 
   console.log('users2=', users2);
+
+  // fetch by email
+  let userByEmail = await UserModel.find({
+      email: 'Travon35@hotmail.com'
+    },
+    { index: 'gs1' }
+  );
+  console.log(userByEmail)
 }
+
 
 (async () => {
   query()
